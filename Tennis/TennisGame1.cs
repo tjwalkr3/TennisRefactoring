b01fe7aa1820d8abe506d8cda3607412dd757329
Thomas Jones <tjwalkr3@gmail.com>
2025-10-27 14:22:55 -0600

    refactoring day 1

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -13,6 +13,7 @@ namespace Tennis
             this.player2Name = player2Name;
         }
 
+        // scorepoint
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
@@ -27,55 +28,83 @@ namespace Tennis
             var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = GetScoreWhenTied();
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = GetScoreWhenAdvantaged();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                GetScoreWhenNotTiedOrAdvantaged(ref score, ref tempScore);
             }
             return score;
+        }
+
+        private string GetScoreWhenTied()
+        {
+            string score;
+            switch (m_score1)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+
+            }
+
+            return score;
+        }
+
+        private string GetScoreWhenAdvantaged()
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private void GetScoreWhenNotTiedOrAdvantaged(ref string score, ref int tempScore)
+        {
+            for (var i = 1; i < 3; i++)
+            {
+                tempScore = GetBaseScore(ref score, i);
+            }
+        }
+
+        private int GetBaseScore(ref string score, int playerNumber)
+        {
+            int tempScore;
+            if (playerNumber == 1) tempScore = m_score1;
+            else { score += "-"; tempScore = m_score2; }
+            switch (tempScore)
+            {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
+            }
+
+            return tempScore;
         }
     }
 }
